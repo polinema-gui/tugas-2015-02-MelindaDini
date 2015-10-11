c3afb0b614541164be17447199f25f9c79d1740a
MelindaDini <melindadininingtyas@gmail.com>
2015-10-11 08:49:37 +0700

    kalkulator

--- tugas-2015-02/Form1.Designer.vb
+++ tugas-2015-02/Form1.Designer.vb
@@ -22,9 +22,248 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.SamaDengan = New System.Windows.Forms.Button()
+        Me.tambah = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.kurang = New System.Windows.Forms.Button()
+        Me.ButtonHapus = New System.Windows.Forms.Button()
+        Me.ButtonHasil = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.Location = New System.Drawing.Point(14, 231)
+        Me.Button0.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(52, 40)
+        Me.Button0.TabIndex = 1
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'SamaDengan
+        '
+        Me.SamaDengan.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SamaDengan.Location = New System.Drawing.Point(82, 231)
+        Me.SamaDengan.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.SamaDengan.Name = "SamaDengan"
+        Me.SamaDengan.Size = New System.Drawing.Size(112, 40)
+        Me.SamaDengan.TabIndex = 2
+        Me.SamaDengan.Text = "="
+        Me.SamaDengan.UseVisualStyleBackColor = True
+        '
+        'tambah
+        '
+        Me.tambah.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tambah.Location = New System.Drawing.Point(208, 229)
+        Me.tambah.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tambah.Name = "tambah"
+        Me.tambah.Size = New System.Drawing.Size(58, 40)
+        Me.tambah.TabIndex = 3
+        Me.tambah.Text = "+"
+        Me.tambah.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(14, 53)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(52, 53)
+        Me.Button7.TabIndex = 4
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(14, 113)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(52, 54)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(14, 174)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(52, 49)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(82, 174)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(49, 49)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(82, 114)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(49, 54)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(83, 52)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(49, 54)
+        Me.Button8.TabIndex = 9
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(146, 114)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(49, 54)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(146, 52)
+        Me.Button9.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(49, 54)
+        Me.Button9.TabIndex = 11
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(146, 172)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(49, 54)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'kurang
+        '
+        Me.kurang.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kurang.Location = New System.Drawing.Point(208, 172)
+        Me.kurang.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.kurang.Name = "kurang"
+        Me.kurang.Size = New System.Drawing.Size(58, 49)
+        Me.kurang.TabIndex = 13
+        Me.kurang.Text = "-"
+        Me.kurang.UseVisualStyleBackColor = True
+        '
+        'ButtonHapus
+        '
+        Me.ButtonHapus.Location = New System.Drawing.Point(208, 53)
+        Me.ButtonHapus.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ButtonHapus.Name = "ButtonHapus"
+        Me.ButtonHapus.Size = New System.Drawing.Size(58, 114)
+        Me.ButtonHapus.TabIndex = 14
+        Me.ButtonHapus.Text = "Hapus"
+        Me.ButtonHapus.UseVisualStyleBackColor = True
+        '
+        'ButtonHasil
+        '
+        Me.ButtonHasil.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonHasil.Location = New System.Drawing.Point(14, 13)
+        Me.ButtonHasil.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ButtonHasil.Multiline = True
+        Me.ButtonHasil.Name = "ButtonHasil"
+        Me.ButtonHasil.Size = New System.Drawing.Size(252, 30)
+        Me.ButtonHasil.TabIndex = 15
+        Me.ButtonHasil.Text = "0"
+        Me.ButtonHasil.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(18, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(217, 27)
+        Me.TextBox1.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 285)
+        Me.Controls.Add(Me.ButtonHasil)
+        Me.Controls.Add(Me.ButtonHapus)
+        Me.Controls.Add(Me.kurang)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.tambah)
+        Me.Controls.Add(Me.SamaDengan)
+        Me.Controls.Add(Me.Button0)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents SamaDengan As System.Windows.Forms.Button
+    Friend WithEvents tambah As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents kurang As System.Windows.Forms.Button
+    Friend WithEvents hapus As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonHapus As System.Windows.Forms.Button
+    Friend WithEvents ButtonHasil As System.Windows.Forms.TextBox
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 
+    Private Sub ButtonHasil_TextChanged(sender As Object, e As EventArgs) Handles ButtonHasil.TextChanged
+
+    End Sub
 End Class
--- tugas-2015-02/Form1.vb
+++ tugas-2015-02/Form1.vb
@@ -1,3 +1,140 @@
 ﻿Public Class Form1
 
+    Dim nilai1, nilai2 As Long
+    Dim hasil As Integer
+    Dim operasi, txtnilai1, txtnilai2, txtnilai3 As String
+   
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "0"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "0"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "1"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "1"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "2"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "2"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "3"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "3"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "4"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "4"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "5"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "5"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "6"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "6"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "7"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "7"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "8"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "8"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "9"
+            ButtonHasil.Text = txtnilai1
+        Else
+            txtnilai2 = txtnilai2 + "9"
+            ButtonHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub ButtonHapus_Click(sender As Object, e As EventArgs) Handles ButtonHapus.Click
+        operasi = ""
+        txtnilai1 = ""
+        txtnilai2 = ""
+        ButtonHasil.Clear()
+    End Sub
+
+    Private Sub kurang_Click(sender As Object, e As EventArgs) Handles kurang.Click
+        operasi = "-"
+    End Sub
+
+    Private Sub tambah_Click(sender As Object, e As EventArgs) Handles tambah.Click
+        operasi = "+"
+    End Sub
+
+    Private Sub SamaDengan_Click(sender As Object, e As EventArgs) Handles SamaDengan.Click
+        nilai1 = Val(txtnilai1)
+        nilai2 = Val(txtnilai2)
+        If operasi = "+" Then
+            hasil = nilai1 + nilai2
+            ButtonHasil.Text = hasil
+        ElseIf operasi = "-" Then
+            hasil = nilai1 - nilai2
+            ButtonHasil.Text = hasil
+        End If
+
+        operasi = ""
+        txtnilai1 = ""
+        txtnilai2 = ""
+        txtnilai1 = Str(hasil)
+    End Sub
 End Class
+
+
